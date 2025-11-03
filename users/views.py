b6a111aa02715478eef292870e05d7cd79fa8b2d
Anechka <maidankina.av@dvfu.ru>
2025-11-03 13:15:33 +0900

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm, LoginForm
-
 def register_view(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -13,7 +12,6 @@ def register_view(request):
     else:
         form = RegistrationForm()
     return render(request, 'users/register.html', {'form': form})
-
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -24,6 +22,8 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect('home')
+            else:
+                form.add_error(None, 'Неверное имя пользователя или пароль')
     else:
         form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
@@ -32,4 +32,4 @@ def logout_view(request):
     return redirect('home')
 @login_required
 def profile_view(request):
-    return render (request, 'users/profile.html', {'user': request.user})
+    return render(request, 'users/profile.html', {'user': request.user})
